e19b8c670c2d7814f8585e8dc3098ff731f8735a
Muhammad <muhammadbhaska0@gmail.com>
2021-07-05 04:25:57 +0000

    feat: added textfield component

---
+++ src/components/Card/ProductCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface ProductCardProps {}
+
+const ProductCard = (props: ProductCardProps) => {
+  return <div></div>;
+};
+
+export default ProductCard;
--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,7 +1,36 @@
-interface TextFieldProps {}
+import { Flex, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { ReactNode } from 'react';
 
-const TextField = (props: TextFieldProps) => {
-  return <div></div>;
+interface TextFieldProps {
+  leftIcon: ReactNode;
+  placeholder: string;
+}
+
+const TextField = ({ leftIcon, placeholder }: TextFieldProps) => {
+  return (
+    <Flex w="100%" alignItems="center" my={4} flexGrow={1}>
+      <InputGroup>
+        {leftIcon && (
+          <InputLeftElement
+            pointerEvents="none"
+            children={leftIcon}
+            h="100%"
+            w={12}
+          />
+        )}
+
+        <Input
+          w="100%"
+          h="48px"
+          pl={12}
+          bg="blackAlpha.100"
+          borderRadius={15}
+          placeholder={placeholder}
+          focusBorderColor="green.400"
+        />
+      </InputGroup>
+    </Flex>
+  );
 };
 
 export default TextField;
